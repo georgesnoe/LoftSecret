6263d1f1f5a71bf8b330a84055b04384269591cd
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-14 18:16:44 +0000

    Setup views for Login and Signup

---
+++ LoftSecret/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoftSecret.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ LoftSecret/Controllers/SignupController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoftSecret.Controllers
+{
+    public class SignupController : Controller
+    {
+        // GET: SignupController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
