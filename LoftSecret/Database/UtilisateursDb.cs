5c27dd423cbabcb0fa03c4b3a00b8e84ca282e80
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-15 18:17:48 +0000

    Added method AddUtilisateur and removed FindUtilisateurById

--- LoftSecret/Database/UtilisateursDb.cs
+++ LoftSecret/Database/UtilisateursDb.cs
@@ -1,12 +1,28 @@
+using MySql.Data.MySqlClient;
+
 using LoftSecret.Models;
 
 namespace LoftSecret.Database;
 
 public class UtilisateursDb
 {
-    public static async Task<Utilisateurs?> FindUtilisateurById(int id)
+    /* Add an user into the database with its informations */
+    public static async Task AddUtilisateur(Utilisateurs utilisateur)
     {
-        await Task.Delay(1000);
-        return null;
+        using (var connection = new MySqlConnection(InitDatabase.__connectionString))
+        {
+            await connection.OpenAsync();
+            string query = "INSERT INTO utilisateurs (nom, prenoms, telephone, email, mot_de_passe, date_inscription, role_id) VALUES (@nom, @prenoms, @telephone, @email, @mot_de_passe, @date_inscription, @role_id)";
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@nom", utilisateur.Nom);
+                command.Parameters.AddWithValue("@prenoms", utilisateur.Prenoms);
+                command.Parameters.AddWithValue("@telephone", utilisateur.Telephone);
+                command.Parameters.AddWithValue("@email", utilisateur.Email);
+                command.Parameters.AddWithValue("@mot_de_passe", utilisateur.MotDePasse);
+                command.Parameters.AddWithValue("@date_inscription", utilisateur.DateInscription);
+                command.Parameters.AddWithValue("@role_id", utilisateur.RoleId);
+            }
+        }
     }
 }
