08744c1361767802d77cbe886b1f2d12b30221e2
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-15 18:18:26 +0000

    Removed date_inscription because not needed

--- LoftSecret/Database/UtilisateursDb.cs
+++ LoftSecret/Database/UtilisateursDb.cs
@@ -12,7 +12,7 @@ public class UtilisateursDb
         using (var connection = new MySqlConnection(InitDatabase.__connectionString))
         {
             await connection.OpenAsync();
-            string query = "INSERT INTO utilisateurs (nom, prenoms, telephone, email, mot_de_passe, date_inscription, role_id) VALUES (@nom, @prenoms, @telephone, @email, @mot_de_passe, @date_inscription, @role_id)";
+            string query = "INSERT INTO utilisateurs (nom, prenoms, telephone, email, mot_de_passe, role_id) VALUES (@nom, @prenoms, @telephone, @email, @mot_de_passe, @role_id)";
             using (var command = new MySqlCommand(query, connection))
             {
                 command.Parameters.AddWithValue("@nom", utilisateur.Nom);
@@ -20,7 +20,6 @@ public class UtilisateursDb
                 command.Parameters.AddWithValue("@telephone", utilisateur.Telephone);
                 command.Parameters.AddWithValue("@email", utilisateur.Email);
                 command.Parameters.AddWithValue("@mot_de_passe", utilisateur.MotDePasse);
-                command.Parameters.AddWithValue("@date_inscription", utilisateur.DateInscription);
                 command.Parameters.AddWithValue("@role_id", utilisateur.RoleId);
             }
         }
