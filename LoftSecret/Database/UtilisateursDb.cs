f64e25405b22d648efc780a88add7939b5498f5d
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-15 18:13:00 +0000

    Add nullable to FindUtilisateurById method

--- LoftSecret/Database/UtilisateursDb.cs
+++ LoftSecret/Database/UtilisateursDb.cs
@@ -4,7 +4,7 @@ namespace LoftSecret.Database;
 
 public class UtilisateursDb
 {
-    public static async Task<Utilisateurs> FindUtilisateurById(int id)
+    public static async Task<Utilisateurs?> FindUtilisateurById(int id)
     {
         await Task.Delay(1000);
         return null;
