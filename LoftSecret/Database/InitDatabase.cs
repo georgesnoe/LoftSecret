4c7856399b19f711d68f8a43878f8f8a0b3586d2
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-15 16:58:01 +0000

    Setup Database Init code and Setup UtilisateursDb

---
+++ LoftSecret/Database/InitDatabase.cs
@@ -0,0 +1,27 @@
+namespace LoftSecret.Database;
+
+public class InitDatabase
+{
+    /* MySql Database configuration */
+
+    /* Database server or provider */
+    private static readonly string __dbServer = "localhost";
+
+    /* Database port
+       Note: By default, mysql uses 3306 so
+       left this untouched
+    */
+    private static readonly string __dbPort = "3306";
+
+    /* Database name (eg. myawesomedb) */
+    private static readonly string __dbName = "loft_secret";
+
+    /* Database user (eg. yourname) */
+    private static readonly string __dbUser = "root";
+
+    /* Database password */
+    private static readonly string __dbPasswd = "";
+
+    /* Resulting string */
+    public static readonly string __connectionString = $"Server={__dbServer};Port={__dbPort};Database={__dbName};Uid={__dbUser};Pwd={__dbPasswd}";
+}
---
+++ LoftSecret/Database/UtilisateursDb.cs
@@ -0,0 +1,12 @@
+using LoftSecret.Models;
+
+namespace LoftSecret.Database;
+
+public class UtilisateursDb
+{
+    public static async Task<Utilisateurs> FindUtilisateurById(int id)
+    {
+        await Task.Delay(1000);
+        return null;
+    }
+}
