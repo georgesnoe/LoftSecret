3ac82550e3b042b723de8a9b5a67a3e397969430
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-06-16 10:15:55 +0000

    Create controller to handle Proprietaire related actions

---
+++ LoftSecret/Controllers/ProprietaireController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoftSecret.Controllers;
+
+public class Proprietaire : Controller
+{
+    // GET: /Proprietaire
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
